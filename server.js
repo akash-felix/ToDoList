ae013ef80c750443972db18b1e2f3bc8258433a4
unknown <akash.felix@gmail.com>
2021-04-07 20:08:09 +0530

    ammu s commit

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/list',require('./routes/api/list'));
 app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/subitem',require('./routes/api/subitem'));
 
 const PORT=process.env.PORT ||5000;
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));  
